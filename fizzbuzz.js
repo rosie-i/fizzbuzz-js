8fee1e113e027d6a7f2a3ae4d1c050f2d56273a7
Rosie Inman <rosie.inman@bbc.co.uk>
2021-02-17 14:05:22 +0000

    Add basic FizzBuzz implementation

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,19 @@
 
-
 // Here, we create our main function.
 function fizzbuzz() {
     
-    console.log('Welcome to FizzBuzz!');
+    for (let i = 1; i <= 100; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)){
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0){
+            console.log("Fizz");
+        } else if (i % 5 === 0){
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
 
-    // Put your code here...
-
-    
 }
 
 // Now we run the main function...
